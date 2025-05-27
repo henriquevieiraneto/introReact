75b9c5951e97e494d3509e2cb689c87c287be488
henriquevieiraneto <henriquevieiraneto97@gmail.com>
2025-05-26 22:25:50 -0300

    A

---
+++ ola/src/components/Body.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Body.css'
+
+function Body() {
+    return (
+        <div className='Body'>
+            <h2 className='titulo2'>Galinhas são dinossauros nerfados</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id voluptatum earum voluptatem tenetur sint, officiis voluptatibus consectetur aliquid maxime omnis odit vel maiores nesciunt veritatis eaque facere! Iure, minus expedita?</p>
+        </div>
+            
+        
+    )
+}
+
+export default Body
